95da1b2fd914ffc9e95988b3326561e919a70002
Muratov Amir <amir.myrt@gmail.com>
2022-09-11 01:24:41 +0300

    p1

---
+++ Program.py
@@ -0,0 +1,14 @@
+i = 0
+num3 = 0
+while True:
+    i += 1
+    num = int(input())
+    if i == 1:
+        num2 = num - 1
+    if num > num2:
+        num2 = num
+        num3 = num + num3
+        print(num3)
+
+
+
