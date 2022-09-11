9b21fdebcee31239b2b7831affeda3314626a399
Muratov Amir <amir.myrt@gmail.com>
2022-09-12 01:39:44 +0300

    func_graph

--- Program.py
+++ Program.py
@@ -1,11 +1,17 @@
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
 x = -3.2
 mas = []
-
 while x < 0:
     x = x + 0.2
     y = (0.7 * x ** 3) + (2 * x ** 2) - 1
     mas.append([x,round(y,2)])
+    plt.scatter(x,y)
+plt.savefig('pic_11_1_2.png') 
+plt.show()
+"""
 f = open('C:\chisla.txt','w')
 for ch in mas:
     f.write(str(ch) + "\n")
-f.close()
+f.close()"""
