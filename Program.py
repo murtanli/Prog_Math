9a70591712f194141ecef2082993454e02e30c33
Muratov Amir <amir.myrt@gmail.com>
2022-09-12 02:15:13 +0300

    Добавил функцию

--- Program.py
+++ Program.py
@@ -6,12 +6,7 @@ mas = []
 while x < 0:
     x = x + 0.2
     y = (0.7 * x ** 3) + (2 * x ** 2) - 1
-    mas.append([x,round(y,2)])
+    mas.append(["%.2f" % round(x,2),"%.2f" % round(y,2)])#[x,round(y,2)])
     plt.scatter(x,y)
 plt.savefig('pic_11_1_2.png') 
 plt.show()
-"""
-f = open('C:\chisla.txt','w')
-for ch in mas:
-    f.write(str(ch) + "\n")
-f.close()"""
