068667d70c5eafb0d843f48f4dc80d4669765e11
Muratov Amir <amir.myrt@gmail.com>
2022-09-15 18:18:23 +0300

    exercise_2_2

---
+++ Pr_2_2.py
@@ -0,0 +1,19 @@
+import math as mt
+
+def du(x):
+    return (2 * mt.sin(x) ** 2) / 3 - (3 * mt.cos(x) ** 2) / 4
+
+eps = 1/(10*10*10*10*10)
+
+a = 0
+b = mt.pi/2
+
+while True:
+    c = (a + b) / 2
+    if du(c) * du(b) > 0:
+        b = c
+    else:
+        a = c
+    if b - a < eps:
+        break
+print(round(a,5))
