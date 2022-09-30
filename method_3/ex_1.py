d5bb417c99d5e7744599f566f43e0ea3a42b6d9f
murtanli <70170580+murtanli@users.noreply.github.com>
2022-10-01 02:15:01 +0300

    ex_1

--- method_3/ex_1.py
+++ method_3/ex_1.py
@@ -1,16 +1,14 @@
 import math as mt
-
 def fun(x):
     return mt.tan(x) - x
-
+def fun2(x):
+    return (1/(mt.cos(x))**2) - 1
 eps = 1/10*10*10*10*10
 
-x1 = 4.67
+xn = 4.67
+
 while True:
-    x0 = x1
-    x1 = x0-fun(x0) / fun(x0)
-
-    if abs(x0 - x1) <= eps:
-        print(x1,fun(x1))
+    xn = xn - (fun(xn)/fun2(xn))
+    if abs(xn - fun(xn)) < eps:
+        print(xn)
         break
-
