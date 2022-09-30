39b69fa0d7d91159b2e5b26906347a77fcfa354a
murtanli <70170580+murtanli@users.noreply.github.com>
2022-10-01 00:51:46 +0300

    Met_2_2

--- method_2/Method_2_2.py
+++ method_2/Method_2_2.py
@@ -1,12 +1,12 @@
 import math as mt
 
 def fun1(x):
-    return x + mt.log(x + 0.5) + 0.5
+    return (x**4) + (2*x)**3 - x - 1
 
 eps = 0.00001
 
-xn = 0
-xk = 2
+xn = 0.1
+xk = 1
 
 while True:
     p = (fun1(xk)*xn-fun1(xn)*xk)/(fun1(xk) - fun1(xn))
